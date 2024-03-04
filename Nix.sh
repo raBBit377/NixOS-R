ed4d39979c20e73b6ffd78e4bacd54076aebbd44
rabbit <98951474+raBBit377@users.noreply.github.com>
2024-03-04 23:11:31 +0200

    Update Nix.sh

--- Nix.sh
+++ Nix.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
-# Змінні
-disk="/dev/sda"
-esp_size="5.4gb"
-swap_size="50G"
-btrfs_start="4.5GiB"
+# Виконання команд по черзі
 
-# Створення ESP (EFI System Partition)
-parted "$disk" -- mklabel gpt
-parted "$disk" mkpart esp fat32 0% "$esp_size"
-parted "$disk" set 1 esp on
-parted "$disk" set 1 boot on
-mkfs.fat -F 32 -n UEFI "${disk}1"
+sudo su
 
-# Створення Swap
-parted "$disk" mkpart swap linux-swap "$esp_size" +"$swap_size"
-mkswap -L SWAP "${disk}2"
+# Встановлення необхідного пакету
+nix-shell -p btrfs-progs
 
-# Створення BTRFS
-parted "$disk" mkpart nixos btrfs "$btrfs_start" 100%
-mkfs.btrfs -f -L NIXOS "${disk}3"
+# Створення розділів на диску
+parted /dev/sda -- mklabel gpt
+parted /dev/sda mkpart esp fat32 0% 5.4gb
+parted /dev/sda set 1 esp on
+parted /dev/sda set 1 boot on
+parted /dev/sda mkpart swap linux-swap 512mb 4.5gb
+parted /dev/sda mkpart nixos btrfs 4.5GiB 100%
+
+# Форматування файлових систем
+mkfs.fat -F 32 -n UEFI /dev/sda1
+mkswap -L SWAP /dev/sda2
+mkfs.btrfs -f -L NIXOS /dev/sda3
 
 # BTRFS subvolumes
-mount -t btrfs "${disk}3" /mnt
+mount -t btrfs /dev/sda3 /mnt
 btrfs subvolume create /mnt/@root
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@nix
 umount /mnt
 
-# Монтування
-mount -t btrfs -o noatime,compress=zstd,subvol=@root "${disk}3" /mnt
+# Монтування файлових систем
+mount -t btrfs -o noatime,compress=zstd,subvol=@root /dev/sda3 /mnt
 mkdir -p /mnt/{boot,nix,home}
-mount -t btrfs -o noatime,compress=zstd,subvol=@home "${disk}3" /mnt/home
-mount -t btrfs -o noatime,compress=zstd,subvol=@nix "${disk}3" /mnt/nix
-mount -t vfat "${disk}1" /mnt/boot
-swapon "${disk}2"
+mount -t btrfs -o noatime,compress=zstd,subvol=@home /dev/sda3 /mnt/home
+mount -t btrfs -o noatime,compress=zstd,subvol=@nix /dev/sda3 /mnt/nix
+mount -t vfat /dev/sda1 /mnt/boot
+swapon /dev/sda2
 
-echo "Готово! Розділений диск та налаштовано файлові системи."
-
-
+echo "Всі команди виконані послідовно."
 
 
