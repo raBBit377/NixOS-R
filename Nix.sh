3254adc7b50601d2e8c9cf2c1b4946eae75ff937
rabbit <98951474+raBBit377@users.noreply.github.com>
2024-03-05 11:42:06 +0200

    Update Nix.sh

--- Nix.sh
+++ Nix.sh
@@ -6,30 +6,30 @@ parted -s /dev/sda -- mklabel gpt
 ## MARKING
 
 # BOOT
-parted -s /dev/sdX mkpart esp fat32 0% 5.4gb
-parted -s /dev/sdX set 1 esp on
-parted -s /dev/sdX set 1 boot on
-mkfs.fat -F 32 -n UEFI /dev/sdXY
+parted -s /dev/sda mkpart esp fat32 0% 5.4gb
+parted -s /dev/sda set 1 esp on
+parted -s /dev/sda set 1 boot on
+mkfs.fat -F 32 -n UEFI /dev/sda1
 
 # SWAP
-parted -s /dev/sdX mkpart swap linux-swap 512mb 44.5gb
-mkswap -L SWAP /dev/sdXW
+parted -s /dev/sda mkpart swap linux-swap 512mb 44.5gb
+mkswap -L SWAP /dev/sda2
 
 # BTRFS
-parted -s /dev/sdX mkpart nixos btrfs 4.5GiB 100%
-mkfs.btrfs -f -L NIXOS /dev/sdXZ
+parted -s /dev/sda mkpart nixos btrfs 4.5GiB 100%
+mkfs.btrfs -f -L NIXOS /dev/sda3
 
 ## BTRFS subvolumes
-mount -t btrfs /dev/sdXZ /mnt
+mount -t btrfs /dev/sda3 /mnt
 btrfs subvolume create /mnt/@root
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@nix
 umount /mnt
 
 ## MOUNT
-mount -t btrfs -o noatime,compress=zstd,subvol=@root /dev/sdXZ /mnt
+mount -t btrfs -o noatime,compress=zstd,subvol=@root /dev/sda3 /mnt
 mkdir -p /mnt/{boot,nix,home}
-mount -t btrfs -o noatime,compress=zstd,subvol=@home /dev/sdXZ /mnt/home
-mount -t btrfs -o noatime,compress=zstd,subvol=@nix /dev/sdXZ /mnt/nix
+mount -t btrfs -o noatime,compress=zstd,subvol=@home /dev/sda3 /mnt/home
+mount -t btrfs -o noatime,compress=zstd,subvol=@nix /dev/sda3 /mnt/nix
 mount -t vfat /dev/sda1 /mnt/boot
-swapon /dev/sdXW
+swapon /dev/sda2
